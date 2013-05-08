3c0d3b7ce210366edadd125ef2a814908e68e8c8
Jesus Vasquez <jesusvasquez333@gmail.com>
2013-05-08 19:04:23 +0200

    Add maximum current set

--- motor.c
+++ motor.c
@@ -1,5 +1,6 @@
 #include <bcm2835.h>
 #include <stdio.h>
+#include <string.h>
 
 #define	CONTROL_VER_1
 
@@ -75,9 +76,9 @@ int help(char *command)
 	
 	printf("Developed by Jesus Vasquez.\n");
 	#ifdef CONTROL_VER_1
-		printf("Usage: %s <MODE> <SPEED> <ACCE> <ACCE_F>\n Where:\n  <MODE> 0 = 16 microsteps; 1 = 8 microsteps; 2 = 4 microsteps; 3 = 2 microsteps;\n  <SPEED> 0 = 1875 rpm; 1 = 937,5 rpm; ... ; 7 = 14,6 rpm; ... ; 15 = 0,06 rpm.\n  <ACCE> 0 = without acceleration curve; 1 = with acceleration curve. and\n  <ACCE_F> = acceleration factor (1..5)\n", command);
+		printf("Usage: %s <MODE> <SPEED> <ACCE> <ACCE_F> <CURRENT_MAX>\n Where:\n  <MODE> 0 = 16 microsteps; 1 = 8 microsteps; 2 = 4 microsteps; 3 = 2 microsteps;\n  <SPEED> 0 = 1875 rpm; 1 = 937,5 rpm; ... ; 7 = 14,6 rpm; ... ; 15 = 0,06 rpm.\n  <ACCE> 0 = without acceleration curve; 1 = with acceleration curve.\n  <ACCE_F> = acceleration factor (1..5). and\n  <CURRENT_MAX> 0 = 3,0 A ; 1 = 2,5 A ; 2 = 2,0 A ; 3 = 1,5 A ; 4 = 1,0 A ; 5 = 0,5 A\n", command);
 	#else
-		printf("Usage: %s <MODE> <SPEED> <ACCE> <ACCE_F>\n Where:\n  <MODE> 0 = 32 microsteps; 1 = 16 microsteps; 2 = 8 microsteps; 3 = 4 microsteps;\n  <SPEED> 0 = 937,5 rpm; 1 = 468,8 rpm; ... ; 7 = 7,32 rpm; ... ; 15 = 0,03 rpm.\n  <ACCE> 0 = without acceleration curve; 1 = with acceleration curve. and\n  <ACCE_F> = acceleration factor (1..5)\n", command);
+		printf("Usage: %s <MODE> <SPEED> <ACCE> <ACCE_F> <CURRENT_MAX>\n Where:\n  <MODE> 0 = 32 microsteps; 1 = 16 microsteps; 2 = 8 microsteps; 3 = 4 microsteps;\n  <SPEED> 0 = 937,5 rpm; 1 = 468,8 rpm; ... ; 7 = 7,32 rpm; ... ; 15 = 0,03 rpm.\n  <ACCE> 0 = without acceleration curve; 1 = with acceleration curve.\n  <ACCE_F> = acceleration factor (1..5).and\n  <CURRENT_MAX> 0 = 3,0 A ; 1 = 2,5 A ; 2 = 2,0 A ; 3 = 1,5 A ; 4 = 1,0 A ; 5 = 0,5 A\n", command);
 	#endif
 	return 1;
 }
@@ -98,9 +99,15 @@ int main(int argc, char **argv)
 	uint8_t speed_var = 8;
 	uint16_t delay_var, delay_var_final;
 	uint16_t speedchange_count = 0;
-	uint8_t use_acceleration, acceleration_factor;
+	uint8_t use_acceleration, acceleration_factor, current_index;
 	
-	uint8_t dac_vals[] = {0, 1, 3, 4, 6, 7, 8, 10, 11, 12, 12, 13, 14, 14, 15, 15, 15};
+	uint8_t dac_vals[17];
+	uint8_t dac_vals_all[] = {	 0,  1,  3,  4,  6,  7,  8, 10, 11, 12, 12, 13, 14, 14, 15, 15, 15, \
+								 0,  1,  2,  4,  5,  6,  7,  8,  9, 10, 11, 11, 12, 12, 13, 13, 13, \
+								 0,  1,  2,  3,  4,  5,  6,  6,  7,  8,  8,  9,  9, 10, 10, 10, 10, \
+								 0,  1,  1,  2,  3,  4,  4,  5,  5,  6,  6,  7,  7,  7,  8,  8,  8, \
+								 0,  1,  1,  1,  2,  2,  3,  3,  4,  4,  4,  5,  5,  5,  5,  5,  5, \
+								 0,  0,  0,  1,  1,  1,  1,  2,  2,  2,  2,  2,  2,  2,  3,  3,  3 	};
 
 	#ifdef CONTROL_VER_1
 		char *mode_str[] = {"Hexal step (3200 microstep/rev)", "Octal Step (1600 microstep/rev)", "Quarter Step (800 microstep/rev)", "Double Step (400 microstep/rev)"};
@@ -109,8 +116,9 @@ int main(int argc, char **argv)
 		char *mode_str[] = {"Hexal step (6400 microstep/rev)", "Octal Step (3200 microstep/rev)", "Quarter Step (1600 microstep/rev)", "Double Step (800 microstep/rev)"};
 		char *speed_str[] = {"937,5 rpm", "468,75 rpm", "234,38 rpm", "117,19 rpm", "58,59 rpm", "29,30 rpm", "14,65 rpm", "7,32 rpm", "3,66 rpm", "1,83 rpm", "0,92 rpm", "0,46 rpm", "0,23 rpm", "0,11 rpm", "0,06 rpm", "0,03 rpm"};
 	#endif
+		char *current_str[] = {"3,0 A", "2,5 A", "2,0 A", "1,5 A", "1,0 A", "0,5 A" };
 	
-	if (argc != 5)
+	if (argc != 6)
 		return help(argv[0]);
 	
 	mode = atoi(argv[1]); 
@@ -128,7 +136,11 @@ int main(int argc, char **argv)
 	acceleration_factor = atoi(argv[4]); 
 	if ((acceleration_factor  < 1) | (acceleration_factor  > 5))
 		return help(argv[0]);
-		
+	
+	current_index = atoi(argv[5]);
+	if (current_index  > 5) 
+		return help(argv[0]);
+	
     if (!bcm2835_init())
     {
     	printf("Error during BCM2835_INIT() function\n");
@@ -147,6 +159,8 @@ int main(int argc, char **argv)
 	else
 		delay_var = (1 << speed);
 
+	memcpy(dac_vals, dac_vals_all + 17*current_index, 17);
+	
 	microstep_time = 10 * delay_var * (uint16_t)(1 << mode);
 	lookuptable_delta = (uint8_t)(1 << mode);
 	microstep_max = (uint8_t)(1 << (4 - mode));
@@ -162,6 +176,7 @@ int main(int argc, char **argv)
 	printf("Speed selected = %s\n", speed_str[speed]);
 	printf("Use acceleration curve = %d\n", use_acceleration);
 	printf("Acceleration factor = %d\n", acceleration_factor);
+	printf("Maximux current set = %s\n\n", current_str[current_index]);
 	
     // Set the pin to be an output
     bcm2835_gpio_fsel(WINDA_M1, BCM2835_GPIO_FSEL_OUTP);
